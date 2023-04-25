833b701721c44b112eb245dac75648565a63bd6d
Hugo Full <hugocatalan2@gmail.com>
2023-04-25 14:28:13 -0300

    nmb

--- src/app/components/acerca-de/edit-acerca-de.component.ts
+++ src/app/components/acerca-de/edit-acerca-de.component.ts
@@ -30,7 +30,7 @@ export class EditAcercaDeComponent implements OnInit {
 
   onUpdate(): void{
     const id = this.activatedRouter.snapshot.params['id'];
-    this.persona.img = this.imageService.url
+    this.persona.img = this.imageService.URL
     this.personaService.update(id, this.persona).subscribe(
       data => {
         this.router.navigate(['']);
